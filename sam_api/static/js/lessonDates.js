1100fd6c732595e01f10bbe9cd2f458ee4df774a
philbowden <pbowden@mail.greenriver.edu>
2020-05-15 23:26:46 -0700

    lessonDates.js is able to populate the admin.html page with all the occurences of the current day when page is initially loaded.

--- sam_api/static/js/lessonDates.js
+++ sam_api/static/js/lessonDates.js
@@ -1,61 +1,88 @@
+ let today = new Date();
+ let currentDay = today.getDay();
+ let currentMonth = today.getMonth();
+ let currentYear = today.getFullYear();
 
-let today = new Date();
-let currentMonth = today.getMonth();
-let currentYear = today.getFullYear();
+ let days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+ console.log("select Day: " + day)
+//let dayIndex = getDayInt(day);
 
-let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-let days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-let day = document.getElementById("selectDay");
-let dayIndex = getDayInt(day);
-
-function getDayInt(day){
-    for(let i = 0; i < days.length; i++){
-        if (days[i] === day){
-            return i;
-        }
-    }
+function setDay()
+{
+    let day = document.getElementById("selectDay").value;
+    console.log("Day is Selected is: " + day);
+    let dayNumber = getDayInt(day);
+    displayDates(currentYear, currentMonth, dayNumber);
 }
-if(day !== ''){
-    displayDates(currentYear, currentMonth, dayIndex);
-}
+ function getDayInt(day)
+ {
+     let index = 0;
+     while(days[index] !== day)
+     {
+         index++;
+     }
+     return index;
+ }
 
-function getLessonDays(month, day) {
-    let date = new Date(),
-        lessonDays = [];
+ function getLessonDays(year, month, dayNumber)
+ {
+   let day = new Date(year, month, 1);
+   console.log("Day object: " + day)
+   let dates = [];
+   let daysToFirst = (dayNumber + 7 - day.getDay()) % 7;
+   let firstOf = new Date(day.setDate(day.getDate() + daysToFirst));
 
-    while(date.getDay() !== day){
-        date.setDate(date.getDate() + 1);
-    }
+   while (firstOf.getMonth() === month)
+   {
+     dates.push(new Date(+firstOf));
+     firstOf.setDate(firstOf.getDate() + 7);
+     console.log("dates: " + dates)
+   }
+   return dates;
+ }
 
-    while (date.getMonth() === month){
-        lessonDays.push(new Date(date.getTime()));
-        date.setDate(date.getDate() + 7);
-    }
-    return lessonDays;
 
-}
-function  displayDates(year, month, day) {
-    let idArray = ['lesson1', 'lesson2', 'lesson3','lesson4','lesson5'];
-    let dates = getLessonDays(month, day);
+ function displayDates(year, month, day)
+ {
+     let date_label_ids = ['lesson1', 'lesson2', 'lesson3','lesson4','lesson5'];
+     let dates = getLessonDays(year, month, day);
+     let index = 0;
+     while(dates[index] != null)
+     {
+         let month = dates[index].getUTCMonth();
+         month = formatMonth(month);
+         let day = dates[index].getDate();
+         let id = date_label_ids[index];
+         document.getElementById(id).innerHTML = month + " " + day;
+         index++;
+     }
+ }
+ function formatMonth(date)
+ {
+     console.log("formatMonth => date = " + date)
+     let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+     return months[date];
+ }
 
-}
+ function next()
+ {
+     currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
+     currentMonth = (currentMonth + 1) % 12;
+     displayDates(currentMonth, currentYear);
+ }
 
-function next() {
-    currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
-    currentMonth = (currentMonth + 1) % 12;
-    displayDates(currentMonth, currentYear);
-}
+ function previous()
+ {
+     currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
+     currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
+     displayDates(currentMonth, currentYear);
+ }
 
-function previous() {
-    currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
-    currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
-    displayDates(currentMonth, currentYear);
-}
-
-function jump() {
-    currentYear = parseInt(selectYear.value);
-    currentMonth = parseInt(selectMonth.value);
-    displayDates(currentMonth, currentYear);
+ function jump()
+ {
+     currentYear = parseInt(selectYear.value);
+     currentMonth = parseInt(selectMonth.value);
+     displayDates(currentMonth, currentYear);
 }
 
 
