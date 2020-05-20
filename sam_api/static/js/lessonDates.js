50430ec51974b23007f4961b7d27b846740edb88
philbowden <pbowden@mail.greenriver.edu>
2020-05-19 22:24:32 -0700

    Added more times to admin table.

--- sam_api/static/js/lessonDates.js
+++ sam_api/static/js/lessonDates.js
@@ -10,35 +10,35 @@ function resetDay(dayNumber)
     displayDates(currentYear, currentMonth, dayNumber);
 }
 
+function getDayInt(day)
+{
+    let index = 0;
+    while(days[index] !== day)
+    {
+        index++;
+    }
+    return index;
+}
 
- function getDayInt(day)
- {
-     let index = 0;
-     while(days[index] !== day)
-     {
-         index++;
-     }
-     return index;
+function getLessonDays(year, month, dayNumber)
+{
+    let firstDayOfThisMonth = new Date(year, month, 1);
+    let firstDayOfThisMonthIndex = firstDayOfThisMonth.getDay();
+    let dates = [];
+    let daysToFirst = (dayNumber + 7 - firstDayOfThisMonthIndex) % 7;
+    let firstOf = new Date(firstDayOfThisMonth.setDate(firstDayOfThisMonth.getDate() + daysToFirst));
+
+    while (firstOf.getMonth() === month)
+    {
+        dates.push(new Date(+firstOf));
+        firstOf.setDate(firstOf.getDate() + 7);
+    }
+    return dates;
  }
 
- function getLessonDays(year, month, dayNumber)
- {
-   let firstDayOfThisMonth = new Date(year, month, 1);
-   let firstDayOfThisMonthIndex = firstDayOfThisMonth.getDay();
-   let dates = [];
-   let daysToFirst = (dayNumber + 7 - firstDayOfThisMonthIndex) % 7;
-   let firstOf = new Date(firstDayOfThisMonth.setDate(firstDayOfThisMonth.getDate() + daysToFirst));
-
-   while (firstOf.getMonth() === month)
-   {
-     dates.push(new Date(+firstOf));
-     firstOf.setDate(firstOf.getDate() + 7);
-   }
-   return dates;
- }
  function displayDates(year, month, dayNumber)
  {
-     let monthString = formatMonth(month);
+     let monthString = formatFullMonth(month);
      document.getElementById('currentdate').innerHTML = monthString + "  " + year;
      let date_label_ids = ['lesson1', 'lesson2', 'lesson3','lesson4','lesson5'];
      let dates = getLessonDays(year, month, dayNumber);
@@ -48,12 +48,11 @@ function resetDay(dayNumber)
      let index = 0;
      while(dates[index] != null)
      {
-         let month = dates[index].getUTCMonth();
-         month = formatMonth(month);
+         let monthAbbreviation = dates[index].getUTCMonth();
+         monthAbbreviation = formatMonthAbriviation(monthAbbreviation);
          let day = dates[index].getDate();
          let id = date_label_ids[index];
-         let datesLength = dates.length;
-         document.getElementById(id).innerHTML = month + " " + day;
+         document.getElementById(id).innerHTML = monthAbbreviation + " " + day;
          index++;
      }
  }
@@ -83,11 +82,17 @@ function resetDay(dayNumber)
 
 
 
- function formatMonth(date)
+ function formatMonthAbriviation(date)
  {
-     let months = ["Jan ", "Feb ", "Mar ", "Apr ", "May ", "June ", "July ", "Aug ", "Sep ", "Oct ", "Nov ", "Dec "];
+     let months = ["Jan ", "Feb ", "Mar ", "Apr ", "May ", "June ", "July ", "Aug ", "Sept ", "Oct ", "Nov ", "Dec "];
      return months[date];
  }
+ function formatFullMonth(date)
+ {
+     let months = ["January ", "February ", "March ", "April ", "May ", "June ", "July ", "August ", "September ", "October ", "November ", "December "];
+     return months[date];
+ }
+
 
  function nextMonth()
  {
@@ -105,32 +110,6 @@ function resetDay(dayNumber)
      displayDates(currentYear, currentMonth, currentDay);
  }
 
- function jump()
- {
-     currentYear = parseInt(selectYear.value);
-     currentMonth = parseInt(selectMonth.value);
-     displayDates(currentMonth, currentYear);
-}
 
 
-/*
-function getLessonDatesArray(day) {
-            let lessonDatesArray = [];
-            let date = new Date();
-            let month = date.getMonth();
-            while(date.getMonth() === month)
-            if(date.getDay() === day) {
-                lessonDatesArray.push(new Date(date));
-            }
-            date.setDate((date.getDate() + 1));
-            return lessonDatesArray;
-        }
 
-function formatLessonDate(date) {
-            let d = new Date(date),
-                month = '' + (d.getMonth()),
-                day = '' + d.getDate();
-                let monthString = months[month];
-
-            return [monthString, day].join('/');
-        }*/
