b0ff82f94cf39f6208edeba9012e990a219dafd7
philbowden <phil.g.bowden@gmail.com>
2021-03-30 11:18:06 -0700

    date picker

--- sam_api/rm_sam/teacherCal.py
+++ sam_api/rm_sam/teacherCal.py
@@ -6,20 +6,19 @@ root.title('rocknmore.com')
 root.iconbitmap('c:/gui/rocknmore.ico')
 root.geometry("600x400")
 
-def grab_date():
-    selectedDate = cal.get_date()
-    #my_label.config(text=cal.get_date())
-
 cal = Calendar(root, selectmode="day", year=2021, month=3, day=30)
 cal.pack(pady=20, fill="both", expand=True)
 
-openCal = Button(root, text="Select Date", command=grab_date)
-openCal.pack(pady=80)
+
+def grab_date():
+    my_label.config(text=cal.get_date())
+    # selectedDate = cal.get_date()
+
+
+my_button = Button(root, text="Select Date", command=grab_date)
+my_button.pack(pady=20)
 
 my_label = Label(root, text="")
 my_label.pack(pady=20)
 
 root.mainloop()
-
-
-
